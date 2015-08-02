a622c63bd517ab896f13cd6881e61e909ef530ce
BruceSherwood <bruce.sherwood@gmail.com>
2015-08-02 17:46:46 -0600

    Fixed some bugs in the new simple editor

--- ide/editor.js
+++ ide/editor.js
@@ -7,15 +7,16 @@ var hmargin = 50
 window.onload = function init() {
 	var w = window.innerWidth - wmargin
 	var h = window.innerHeight -hmargin
-	linenumbersarea = $('<textarea id=linenumbers/>').appendTo($('body')).css('font-family', 'Verdana', 		 
-        'Sans-Serif').css('overflow', 'hidden').css('font-size', '10pt').css('float',
+	linenumbersarea = $('<textarea id=linenumbers></textarea>').appendTo($('body')).css('font-family', 
+		'Verdana', 'Sans-Serif').css('overflow', 'hidden').css('font-size', '10pt').css('float',
 		'left').css('width', '50px').css('height', h).css('resize', 'none').css('text-align',
 		'right').css('padding-right', '5px').css('background-color', '#EEEEEE').css('border',
 		'0').css('outline-width', '0')
+	linenumbersarea.attr('readonly', true)
 	var info = $('body').append('<div></div>')
-	editarea = $('<textarea id=edit/>').appendTo(info).css('font-family', 'Verdana', 
+	editarea = $('<textarea id=edit wrap="off"></textarea>').appendTo(info).css('font-family', 'Verdana', 
 	    'Sans-Serif').css('font-size', '10pt').css('width', w).css('height', h).css('resize', 
-		'none').css('white-space', 'nowrap').css('border', '0').css('outline-width', '0')
+		'none').css('border', '0').css('outline', 'none')
 	linenumbersarea.val('1')
 	var s = 'GlowScript 1.1 VPython\n'
 	editarea.val(s)
@@ -32,7 +33,6 @@ $(window).resize(function () {
 var update = function() {
 	var lines = editarea.val().split('\n').length
 	var numbers = linenumbersarea.val().split('\n').length
-	console.log(lines, numbers)
 	var s = ''
 	if (lines != numbers) {
 		if (lines > numbers) {
