0643137b08b8a03eae16b3b6dbe06cc0a76e4f4b
BruceSherwood <bruce.sherwood@gmail.com>
2017-10-29 16:53:37 -0400

    label now accepts html codes <b>, <i>, <sub>, <sup>, <br>

--- untrusted/run.js
+++ untrusted/run.js
@@ -4,6 +4,7 @@ window.glowscript_libraries = { // used for unpackaged (X.Ydev) version
     run: [
         "../lib/jquery/2.1/jquery.mousewheel.js",
         "../lib/flot/jquery.flot.min.js",
+        //"../lib/flot/jquery.flot.js",
         "../lib/flot/jquery.flot.crosshair_GS.js",
         "../lib/flot/jquery.flot.axislabels.js",
 //        "../lib/micromarkdown.min.js", // markdown, not ready to use yet
