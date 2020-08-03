0e9e264b1c0db7e2a227b69ad523f75a57423bbc
Steve Spicklemire <steve@spvi.com>
2020-08-03 09:15:25 -0400

    try a constant session key

--- ide/__init__.py
+++ ide/__init__.py
@@ -8,7 +8,7 @@ import uuid
 from flask import Flask
 
 app = Flask(__name__, static_folder='../static', static_url_path='/')
-app.secret_key = str(uuid.uuid4())
 
-from . import routes, models, auth
+from . import routes, models, auth, secret
 
+app.secret_key = secret.FN_SECRET_KEY
