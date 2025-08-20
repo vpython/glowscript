3500af9d7709b9e44057cc495c067071bae167ed
Steve Spicklemire <steve@spvi.com>
2025-08-20 08:52:54 -0400

    making progress.

--- examples/doCheck.js
+++ examples/doCheck.js
@@ -1,2 +1,54 @@
 global.window = {}
-x = require('./package/compiler.3.2.min.js')
+global.RapydScript = require('../package/RScompiler.3.2.min.js')
+debugger;
+
+source = "Web VPython 3.2\n\n\nball=sphere()"
+
+var header = {
+    "version": "3.2",
+    "lang": "vpython",
+    "nodictionary": false,
+    "source": source,
+    "ok": "3.2",
+    "unpackaged": false,
+    "isCurrent": true
+}
+
+var embedHTML = '' // Will be an ampty string if there is a compile error
+var embedScript = window.glowscript_compile(header.source,
+            {lang: header.lang, version: header.version.substr(0,3), 
+            run: false, nodictionary: header.nodictionary})
+console.log('ide 1343', embedScript)
+var divid = "glowscript"
+var main
+var v = Number(header.version.substr(0,3))
+if (v >= 2.9) main = '__main__()' // Starting August 2019, no longer using Streamine
+
+embedScript = ";(function() {" + embedScript + '\n;$(function(){ window.__context = { glowscript_container: $("#' + divid + '")'+remove+' }; '+main+' })})()'
+embedScript = embedScript.replace("</", "<\\/") // escape anything that could be a close script tag... hopefully this sequence only occurs in strings!
+var verdir = "bef1.1"
+if (v == 1.1) verdir = "1.1"
+else if (v >= 2.2) verdir = "2.1"
+else verdir = header.version.substr(0,3)
+var runner = ''
+var exporturl = "https://www."+website+"/"
+if (v >= 2.5 && v < 3.0) exporturl = "https://s3.amazonaws.com/glowscript/"
+// Note: some already exported 3.0 programs contain references to s3.amazonaws.com
+if (header.lang == 'vpython') 
+    runner = '<script type="text/javascript" src="'+exporturl+'package/RSrun.' + header.version + '.min.js"></script>\n'
+embedHTML = ( // embedHTML is a var introduced above to make it easy for downloadHTML
+    '<div id="' + divid + '" class="glowscript">\n' + 
+    '<meta http-equiv="Content-Type" content="text/html; charset=utf-8">\n' +
+    '<link type="text/css" href="'+exporturl+'css/redmond/' + verdir + '/jquery-ui.custom.css" rel="stylesheet" />\n' +
+    '<link type="text/css" href="'+exporturl+'css/ide.css" rel="stylesheet" />\n' + 
+    mathjax +
+    '<script type="text/javascript" src="'+exporturl+'lib/jquery/' + verdir + '/jquery.min.js"></script>\n' +
+    '<script type="text/javascript" src="'+exporturl+'lib/jquery/' + verdir + '/jquery-ui.custom.min.js"></script>\n' +
+    '<script type="text/javascript" src="'+exporturl+'package/glow.' + header.version + '.min.js"></script>\n' +
+    runner +
+    '<script type="text/javascript"><!--//--><![CDATA[//><!--\n\n// START JAVASCRIPT\n' +
+    embedScript + '\n// END JAVASCRIPT\n' +
+    '\n//--><!]]></script>' +
+    '\n</div>');
+
+console.log(embedHTML)
